930700ebaea685e2b358d9d403a421b21a7644eb
Ben Camarillo <jtby97@gmail.com>
2021-12-14 19:44:05 -0600

    work on flower and fungi cards

---
+++ src/components/FlowerCard.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+// import Card from 'react-bootstrap/Card'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function FlowerCards(props) {
+
+  return (
+      <Card style={{ width: '18rem'}}>
+        <Card.body>
+          <Card.Title>Flower Common name</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Flower botany name</Card.Subtitle>
+          <Card.Text>
+            Here is where all the information about the plant will be displayed.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.body>
+      </Card>
+    );
+}
+
+export default FlowerCards
+
